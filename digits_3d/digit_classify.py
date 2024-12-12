247a26e57df065d2c118dcb960d436b8b283fb85
arbinydv <ayadav@maitriservices.com>
2024-12-12 12:16:07 +0200

    improve the code

--- digits_3d/digit_classify.py
+++ digits_3d/digit_classify.py
@@ -1,12 +1,7 @@
 from digits_3d.model_building import train_digit_classifier, predict_digit
 
-def digit_classify(sample, training_data='training_data'):
-    print("Training the model...")
-    train_digit_classifier(training_data)
-
-    print("\nTesting the model with the given sample...")
+def digit_classify(sample):
     predict_digit(sample)
 
 if __name__ == "__main__":
-    # test the classifier with single stroke file
     digit_classify('single_test/stroke_0_0002.csv')
